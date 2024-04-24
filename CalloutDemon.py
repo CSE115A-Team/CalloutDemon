6cd55715f1b1890abf0ac8e6600fc41a5b01c2c3
Rui Zhang <rzhan128@ucsc.edu>
2024-04-23 21:32:20 -0700

    adding labeled map

--- CalloutDemon.py
+++ CalloutDemon.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from tkinter import ttk
-from PIL import ImageTk, Image
+from PIL import ImageTk, Image # make sure install pillow lib
 import os
 
 if __name__ == "__main__":
@@ -13,21 +13,27 @@ if __name__ == "__main__":
     # Map Options
     maps = ['Ascent', 'Bind', 'Icebox', 'Split', 'Lotus', 'Breeze', 'Sunset', 'Ascent']
     maps.sort()
+    labeled_maps = ['ascent', 'bind', 'breeze', 'fracture', 'haven', 'icebox', 'pearl', 'ascent']
+    labeled_maps.sort()
     
     # Create a menu options
     selected_map = StringVar()
     selected_map.set('Ascent')
+    selected_map_labeled = StringVar()
+    selected_map_labeled.set('ascent')
     
     # Creates dropdown
     ttk.OptionMenu(screen, selected_map, *maps).grid(column=0, row=0)
+    ttk.OptionMenu(screen, selected_map_labeled, *labeled_maps).grid(column=10, row=0)
     
     #Map Folder
     unlabeled_map = "Maps"
+    labeled_maps_folder = "labeled_maps"
     
     #Displaying Map
-    def display_image(selected_map):
-        map_name = selected_map.get()
-        map_path = os.path.join(unlabeled_map, map_name.lower() + ".png")
+    def display_image(map, folder):
+        map_name = map.get()
+        map_path = os.path.join(folder, map_name.lower() + ".png")
         if os.path.exists(map_path):
             map = Image.open(map_path)
             map = map.resize((600, 600))
@@ -37,7 +43,9 @@ if __name__ == "__main__":
 
     image_label = ttk.Label(screen)
     image_label.grid(column=1, row=1)
-    selected_map.trace_add('write', lambda *args: display_image(selected_map))
-    display_image(selected_map)
+    selected_map.trace_add('write', lambda *args: display_image(selected_map, unlabeled_map))
+    selected_map_labeled.trace_add('write', lambda *args: display_image(selected_map_labeled, labeled_maps_folder))
+    display_image(selected_map, unlabeled_map)
+    display_image(selected_map_labeled, labeled_maps_folder)
     
     screen.mainloop()
