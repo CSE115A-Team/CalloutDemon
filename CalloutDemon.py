43fb9467f5aaab0366e8773c3d7814db54391db8
Jonathan <jocbaker@ucsc.edu>
2024-04-27 11:56:37 -0700

    Cleaned up the structure of the images and removed redundant dropdown list

--- CalloutDemon.py
+++ CalloutDemon.py
@@ -13,39 +13,32 @@ if __name__ == "__main__":
     # Map Options
     maps = ['Ascent', 'Bind', 'Icebox', 'Split', 'Lotus', 'Breeze', 'Sunset', 'Ascent']
     maps.sort()
-    labeled_maps = ['ascent', 'bind', 'breeze', 'fracture', 'haven', 'icebox', 'pearl', 'ascent']
-    labeled_maps.sort()
     
     # Create a menu options
-    selected_map = StringVar()
-    selected_map.set('Ascent')
-    selected_map_labeled = StringVar()
-    selected_map_labeled.set('ascent')
+    selected_map = StringVar(value = maps[0])
     
     # Creates dropdown
     ttk.OptionMenu(screen, selected_map, *maps).grid(column=0, row=0)
-    ttk.OptionMenu(screen, selected_map_labeled, *labeled_maps).grid(column=10, row=0)
     
     #Map Folder
-    unlabeled_map = "Maps"
-    labeled_maps_folder = "labeled_maps"
+    map_folder = os.path.join("images/maps")
     
     #Displaying Map
-    def display_image(map, folder):
+    def display_image(map: StringVar, folder):
         map_name = map.get()
-        map_path = os.path.join(folder, map_name.lower() + ".png")
+        map_path = os.path.join(folder, map_name.lower() + "_labeled.png")
         if os.path.exists(map_path):
             map = Image.open(map_path)
             map = map.resize((600, 600))
             photo = ImageTk.PhotoImage(map)
             image_label.config(image = photo)
             image_label.image = photo
+            
+    default_map = os.path.join(map_folder, 'ascent_labeled.png')
+    default_map = ImageTk.PhotoImage(Image.open(default_map).resize((600,600)))
 
-    image_label = ttk.Label(screen)
+    image_label = ttk.Label(screen, image=default_map)
     image_label.grid(column=1, row=1)
-    selected_map.trace_add('write', lambda *args: display_image(selected_map, unlabeled_map))
-    selected_map_labeled.trace_add('write', lambda *args: display_image(selected_map_labeled, labeled_maps_folder))
-    display_image(selected_map, unlabeled_map)
-    display_image(selected_map_labeled, labeled_maps_folder)
+    selected_map.trace_add('write', lambda *args: display_image(selected_map, map_folder))
     
     screen.mainloop()
